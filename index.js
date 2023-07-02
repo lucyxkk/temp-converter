b13167d45bdd8ac6a375bd945f663946383086a1
Łucja Kunikowska <lucja.kun2@gmail.com>
2023-07-02 12:39:55 +0200

    Add two dunctions to convert temp

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const celcius = document.getElementById('celcius');
+const fahrenheit = document.getElementById('fahrenheit');
 
-function convert(event){
-    let num = document.getElementById("celcius").value;
-    let result = ((num*1.8)+32);
-    document.getElementById("fahrenheit").value = result;
-    event.preventDefault();
+celcius.addEventListener("input", convertCtoF);
+fahrenheit.addEventListener("input", convertFtoC);
+
+
+function convertCtoF(e) {
+    const num = e.target.value;
+    let result = "";
+
+    if (isNaN(num)) {
+        alert('Not a number')
+    } else {
+        result = ((num*1.8)+32);
+    }
+    
+    fahrenheit.value = result.toFixed(2);
 }
 
+function convertFtoC(e) {
+    const num1 = e.target.value;
+    let result1 = "";
 
+    if (isNaN(num1)) {
+        alert('Not a number')
+    } else {
+        result1 = ((num1-32)*(5/9));
+    }
+    celcius.value = result1.toFixed(2);
+}
